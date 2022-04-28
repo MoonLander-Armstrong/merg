cbc9c97aab937b99ffdccbb6170b1eba4ea523cf
林沛萱 <peihsuan@linpeixuandeMacBook-Air.local>
2022-04-28 16:39:01 +0800

    新增課程表單/create/

--- db/migrate/20220428060731_create_courses.rb
+++ db/migrate/20220428060731_create_courses.rb
@@ -10,3 +10,4 @@ class CreateCourses < ActiveRecord::Migration[6.1]
     end
   end
 end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_28_045133) do
+ActiveRecord::Schema.define(version: 2022_04_28_060731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,8 +63,8 @@ ActiveRecord::Schema.define(version: 2022_04_28_045133) do
     t.index ['chapter_id'], name: 'index_sections_on_chapter_id'
   create_table "courses", force: :cascade do |t|
     t.string "title"
-    t.integer "price"
     t.text "content"
+    t.integer "price"
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
