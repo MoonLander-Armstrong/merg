b2ad3cfbf5d77780f1fd361c26160f1e775bac4f
林沛萱 <peihsuan@linpeixuandeMacBook-Air.local>
2022-04-28 16:39:01 +0800

    新增課程表單/create/

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
 class CoursesController < ApplicationController
-  def new; end
+  
+  def index
+    @courses = Course.all
+  end
+  
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+		if @course.save
+			flash[:notice] = "新增成功!"
+			redirect_to "/courses"
+		else
+			render :new
+		end
+  end
+
+
+  private
+    def course_params
+      params.require(:course).permit(:title, :content, :price, :status)
+    end
 end
