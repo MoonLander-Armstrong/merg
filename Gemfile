142f946e5c6bdf858439715b7b3550394e52a16e
Tgo Wang <ben2021345@hotmail.com>
2022-05-01 12:03:25 +0800

    add backage chapter & section CRUD

--- Gemfile
+++ Gemfile
@@ -19,6 +19,9 @@ gem 'rails', '~> 6.1.5.1'
 gem 'sass-rails', '>= 6'
 gem 'turbolinks', '~> 5'
 gem 'webpacker', '~> 5.0'
+gem 'image_processing', '~> 1.2'
+gem "aws-sdk-s3", require: false
+gem "paranoia", "~> 2.6"
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
 class Section < ApplicationRecord
+  # validates
+  validates :title,
+            presence: true,
+            length: {
+              maximum: 30
+            }
+
+  # relationship
+  belongs_to :chapter
+
+  def publishd_state
+    [
+      %w[
+        草稿 draft
+      ],
+      %w[
+        發布 publish
+      ]
+    ]
+  end
 end
